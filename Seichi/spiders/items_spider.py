9ab57ccbc896a4c99717ec53af9286c4247e7a2e
Grox Ni <nishukai@hotmail.com@github.com>
2019-03-16 12:34:36 -0700

    Data structure refactored

--- Seichi/spiders/items_spider.py
+++ Seichi/spiders/items_spider.py
@@ -8,25 +8,34 @@ import re
 # TODO dynamicly add more jl
 class ItemsSpider(scrapy.Spider):
     name = 'items'
+    key = 0
     with open('./apikey', 'r') as f:
-        key = f.read()
+        api_key = f.read()
 
     def start_requests(self):
         yield scrapy.Request(url='https://legwork.g.hatena.ne.jp/', callback=self.parse)
 
     def parse(self, response):
-        for href in response.css('div.recentsubtitles a::attr(href)')[:15]:
+        for href in response.css('div.recentsubtitles a::attr(href)')[:30]:
             yield response.follow(href, self.parse_detail)
 
     def parse_detail(self, response):
         places = self.standardize(
              response.css('div.body').re('「.*?」'))
-        res = {
-            'title': response.css('span.title::text').get(),
-            'url': unquote(response.request.url),
-            'places': {place: self.get_geo_location(place) for place in places}
-        }
-        yield res
+        title = response.css('span.title::text').get()
+        url = unquote(response.request.url)
+        for place in places:
+            lat, lng = self.get_geo_location(place).values()
+            res = {
+                'key': self.key,
+                'title': title,
+                'placename': place,
+                'url': url,
+                'lat': lat,
+                'lng': lng
+                }
+            self.key += 1
+            yield res
 
     def standardize(self, lst):
         '''
@@ -46,9 +55,9 @@ class ItemsSpider(scrapy.Spider):
         api_url = ('inputtype=' + inputtype +
          '&fields=' + fields +
          '&input=' + placename +
-         '&key=' + self.key)
+         '&key=' + self.api_key)
         result = requests.get(base_url + api_url).json()
         if result['status'] == 'OK':
             return result['candidates'][0]['geometry']['location']
         else:
-            return None
+            return {'lat': None, 'lng': None}
--- seichi-map/src/App.js
+++ seichi-map/src/App.js
@@ -1,43 +1,65 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
-import { Icon } from 'antd';
+import { Icon, Popover } from 'antd';
 
-const data = require('./data/res.json');
+const response = require('./data/res.json');
+
+const Marker = (place) => {
+  const {
+    lat,
+    lng,
+    id,
+    placename,
+    title,
+  } = place;
+  const content = (
+    <div>
+      <p>
+        <strong>Title: </strong>
+        {title}
+      </p>
+      <p>
+        <strong>Latitude: </strong>
+        {Math.abs(lat) + ((lat >= 0) ? '˚ N' : '˚ S')}
+      </p>
+      <p>
+        <strong>Longitude: </strong>
+        {Math.abs(lng) + ((lng >= 0) ? '˚ E' : '˚ W')}
+      </p>
+    </div>
+  );
+  return (
+    <div key={id} lat={lat} lng={lng}>
+      <Popover content={content} title={placename}>
+        <Icon type="pushpin" style={{ fontSize: '10px', color: '#eb2f96' }} />
+      </Popover>
+    </div>
+  );
+};
 
 class App extends Component {
-  static defaultProps = {
-    center: {
-      lat: 35.675888,
-      lng: 139.744858
-    },
-    zoom: 11
-  };
-
-  marklist = (production) => {
-    let {places} = production
-    return Object.keys(places)
-    .filter(k => places[k])
-    .map(k => this.icon(places[k]))
-  }
-
-  icon = (coord) => {
-    console.log(coord)
-    if (coord.lat){
-      return (
-      <Icon type="pushpin" {...coord} style={{ fontSize: '10px', color: '#eb2f96' }}/>
-      )}
-      else {return(<div/>)}
+  constructor() {
+    super();
+    this.state = {
+      center: {
+        lat: 35.675888,
+        lng: 139.744858,
+      },
+      zoom: 11,
+      data: response,
+    };
   }
 
   render() {
+    const { center, zoom, data } = this.state;
     return (
       <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: "AIzaSyAS_gZ-8P6SSW5eru7JCoFTls4vT6fp3KU" }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
+          bootstrapURLKeys={{ key: 'AIzaSyAS_gZ-8P6SSW5eru7JCoFTls4vT6fp3KU' }}
+          defaultCenter={center}
+          defaultZoom={zoom}
         >
-        {data.map(production => this.marklist(production))}
+          {data.filter(place => place.lat).map(place => <Marker {...place} />)}
         </GoogleMapReact>
       </div>
     );
